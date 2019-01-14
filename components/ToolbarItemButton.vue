3acc633258f098104d342e65ee10e3ab12349880
SuuSoJeat <suusojeat@gmail.com>
2019-01-14 15:03:03 +0700

    resort-site-6 Toolbar Button Component

---
+++ components/ToolbarItemButton.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-btn flat class="text-capitalize">
+      <slot/>
+    </v-btn>
+</template>
+
+<script>
+  export default {
+    name: 'ToolbarItemButton'
+  }
+</script>
+
+<style scoped>
+
+</style>
