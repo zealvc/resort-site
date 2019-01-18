ce3a1373a6322a61c080926438e9fdd61f84ad19
Somjot <somjot.pro@gmail.com>
2019-01-18 14:39:20 +0700

    Merge branch 'resort-site-7' of /Users/visal/Desktop/Resort Development with conflicts.

--- pages/index.vue
+++ pages/index.vue
@@ -1,46 +1,121 @@
 <template>
-  <v-container
-    fluid
-    fill-height
-    class="home-hero">
-    <v-layout
-      column
+  <v-layout
+    row
+    wrap
+  >
+    <v-flex
+      xs12
+      md5
+      fill-height
     >
-      <v-flex>
-        <home-toolbar/>
-      </v-flex>
-      <v-layout
-        row
-      >
-        <v-flex
-          md4
-          class="offset-md1 pa-3"
-        >
-          <home-booking-card/>
-        </v-flex>
-      </v-layout>
-    </v-layout>
+      <v-card class="pl-5 pr-5 pt-3 pb-3">
+        <v-card-text class="title">Book accommodations and activities.</v-card-text>
+        <v-form>
+          <v-container>
+            <v-layout
+              column
+            >
+              <v-layout
+                row
+                wrap
+                align-center
+                justify-center
+              >
+                <v-flex
+                  xs12
+                >
+                  <v-text-field
+                    outline
+                    single-line
+                    prepend-inner-icon="search"
+                    placeholder="Meeting at vKirirom"
+                  />
+                </v-flex>
+              </v-layout>
+              <v-layout
+                row
+                wrap
+                align-center
+                justify-center
+                text-xs-center
+              >
+                <v-flex
+                  xs6
+                >
+                  <div class="subheading pb-2 blue-grey--text text--darken-4">Check In</div>
+                  <v-text-field
+                    outline
+                    single-line
+                    placeholder="mm/dd/yy"
+                    class="pr-1"
+                  />
+                </v-flex>
+                <v-flex
+                  xs6
+                >
+                  <div class="subheading pb-2 blue-grey--text text--darken-4">Check Out</div>
+                  <v-text-field
+                    outline
+                    single-line
+                    placeholder="mm/dd/yy"
+                    class="pl-1"
+                  />
+                </v-flex>
+              </v-layout>
+              <v-layout
+                row
+                wrap
+                align-center
+                justify-center
+              >
+                <v-flex
+                  xs12>
+                  <div class="subheading pb-2 blue-grey--text text--darken-4">Guests</div>
+                  <v-select
+                    label="Guests"
+                    outline
+                  />
+                </v-flex>
+              </v-layout>
+              <v-layout
+                row
+                justify-center>
+                <v-btn
+                  depressed
+                  large
+                  class="white--text text-capitalize"
+                  color="#7085F6"
+                >
+                  Search me
+                </v-btn>
+                <v-btn
+                  depressed
+                  large
+                  class="black--text text-capitalize"
+                  color="#7085F6"
+                >
+                  Good
+                </v-btn>
+              </v-layout>
+            </v-layout>
+          </v-container>
+        </v-form>
+      </v-card>
+    </v-flex>
 
-  </v-container>
+  </v-layout>
+
 
 </template>
 
 <script>
-import HomeToolbar from '../components/HomeToolbar'
-import HomeBookingCard from '../components/HomeBookingCard'
+import Logo from '~/components/Logo.vue'
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
 
 export default {
   components: {
-    HomeBookingCard,
-    HomeToolbar
+    Logo,
+    VuetifyLogo
   }
 }
 </script>
-<style scoped>
-.home-hero {
-  background: url('http://source.unsplash.com/0BhSKStVtdM');
-  background-size: cover;
-  width: 100%;
-  height: 100%;
-}
-</style>
