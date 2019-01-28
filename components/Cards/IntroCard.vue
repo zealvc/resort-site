0396a25e25fc92539f56366fbffcf1b94eddcb9a
Metheany Thorn <metheany.thorn@gmail.com>
2019-01-28 21:19:05 +0700

    resort-site-7 Home Page Layout: Created Introducing card component

--- components/Cards/IntroCard.vue
+++ components/Cards/IntroCard.vue
@@ -1,72 +1,60 @@
 <template>
   <div>
-    <div class="px-2">
-      <h2>Introducing of Accommodation</h2>
-    </div>
-    <slick
-      ref="slick"
-      :options="slickOptions">
-      <div
-        v-for="card in cards"
-        :key="card.id">
-        <v-card
-          :href="card.href"
-          flat
-          class="ma-0 rounded-card grey--text text--darken-3"
-          style="background-color: transparent">
-          <v-layout column>
-            <v-flex xs4>
-              <v-img
-                :src="card.src"
-                class="rounded-card"
-                height="150px"/>
-            </v-flex>
-            <v-flex xs8>
-              <v-card-title class="px-0">
-                <div class="blue-grey--text text--darken-3">
-                  <h1 class="body-2 font-weight-thin text-capitalize">Accommodation</h1>
-                  <h1 class="title font-weight-bold text-capitalize">{{ card.title }}</h1>
-                  <h1 class="subheading font-weight-thin text-lowercase">{{ card.price }}$ per night</h1>
-                </div>
-              </v-card-title>
-            </v-flex>
-          </v-layout>
-        </v-card>
-      </div>
-    </slick>
+    <v-card
+      :href="href"
+      flat
+      class="ma-0 rounded-card grey--text text--darken-3"
+      style="background-color: transparent"
+      min-width="267px">
+      <v-layout column>
+        <v-flex xs4>
+          <v-img
+            :src="image"
+            class="rounded-card"
+            height="150px"/>
+        </v-flex>
+        <v-flex xs8>
+          <v-card-title class="px-0 py-2">
+            <div class="blue-grey--text text--darken-3">
+              <div class="body-2 font-weight-thin text- capitalize">{{ category }}</div>
+              <div class="title font-weight-bold text-capitalize">{{ title }}</div>
+              <div class="subheading font-weight-thin text-lowercase">${{ price }} {{ per }}</div>
+            </div>
+          </v-card-title>
+        </v-flex>
+      </v-layout>
+    </v-card>
   </div>
 </template>
 
 <script>
 export default {
-  data: () => ({
-    slickOptions: {
-      dots: false,
-      infinite: false,
-      slidesToShow: 4,
-
-      responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3
-          }
-        },
-        {
-          breakpoint: 940,
-          settings: {
-            slidesToShow: 2
-          }
-        },
-        {
-          breakpoint: 415,
-          settings: {
-            slidesToShow: 1
-          }
-        }
-      ]
+  props: {
+    category: {
+      type: String,
+      default: 'category'
+    },
+    title: {
+      type: String,
+      default: 'title'
+    },
+    image: {
+      type: String,
+      default: '/default_image.png'
+    },
+    price: {
+      type: String,
+      default: '0'
+    },
+    per: {
+      type: String,
+      default: ''
+    },
+    href: {
+      type: String,
+      default: '#'
     }
-  })
+  }
 }
 </script>
 
@@ -74,9 +62,5 @@ export default {
 .rounded-card {
   border-radius: 3px;
   overflow: hidden;
-}
-.card {
-  box-shadow: rgba(0, 0, 0, 0.06) 0px 2px 4px 0px;
-  border: 1px solid lightgray;
 }
 </style>
