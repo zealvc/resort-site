4d2c34b0e94268b9e438a00768cc5c9f00ef3eca
Metheany Thorn <metheany.thorn@gmail.com>
2019-01-29 12:26:52 +0700

    resort-site-7 Home Page Layout: Updated Restaurant component

--- components/Cards/Restaurant.vue
+++ components/Cards/Restaurant.vue
@@ -1,45 +1,26 @@
 <template>
   <div>
-    <div>
-      <h2>Introducing of Restaurant</h2>
-    </div>
+    <div class="headline font-weight-bold">Introducing of Restaurant</div>
     <slick
       ref="slick"
       :options="slickOptions">
-      <div
-        v-for="card in cards"
-        :key="card.id">
-        <v-card
-          :href="card.href"
-          flat
-          class="ma-0 rounded-card grey--text text--darken-3"
-          style="background-color: transparent"
-          min-width="267px">
-          <v-layout column>
-            <v-flex xs4>
-              <v-img
-                :src="card.src"
-                class="rounded-card"
-                height="150px"/>
-            </v-flex>
-            <v-flex xs8>
-              <v-card-title class="px-0 py-2">
-                <div class="blue-grey--text text--darken-3">
-                  <h1 class="body-2 font-weight-thin text-capitalize">Restaurant</h1>
-                  <h1 class="title font-weight-bold text-capitalize">Name of Food</h1>
-                  <h1 class="subheading font-weight-thin text-lowercase">{{ card.price }}$</h1>
-                </div>
-              </v-card-title>
-            </v-flex>
-          </v-layout>
-        </v-card>
-      </div>
+      <intro-card
+        v-for="(post, index) in posts"
+        v-if="index < 4"
+        :key="post.id"
+        :category="post.category"
+        :title="post.title"
+        :price="post.price"
+        :image="post.image"
+        :href="href"/>
     </slick>
   </div>
 </template>
 
 <script>
+import IntroCard from './IntroCard'
 export default {
+  components: { IntroCard },
   data: () => ({
     slickOptions: {
       dots: false,
@@ -49,51 +30,73 @@ export default {
 
       responsive: [
         {
-          breakpoint: 1024,
+          breakpoint: 1224,
           settings: {
-            slidesToShow: 4
+            slidesToShow: 3
           }
         },
         {
-          breakpoint: 940,
+          breakpoint: 1024,
           settings: {
-            slidesToShow: 4
+            slidesToShow: 3
+          }
+        },
+        {
+          breakpoint: 960,
+          settings: {
+            slidesToShow: 2
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 1
           }
         },
         {
           breakpoint: 480,
           settings: {
+            slidesToShow: 1
+          }
+        },
+        {
+          breakpoint: 320,
+          settings: {
             arrows: false,
-            slidesToShow: 4
+            slidesToShow: 1
           }
         }
       ]
     },
-    cards: [
+    posts: [
       {
+        category: 'Restaurant',
         title: 'Luxury Tent',
-        src:
+        image:
           'https://www.bbcgoodfood.com/sites/default/files/guide/guide-image/2018/06/chicken-wings-main.jpg',
         price: '20',
         href: '#'
       },
       {
+        category: 'Restaurant',
         title: 'Khmer Cottage',
-        src:
+        image:
           'http://pandagarden.ca/wp-content/uploads/2017/11/chicken-Chow-Mein_1430566912_1644579089.jpg',
         price: '10',
         href: '#'
       },
       {
+        category: 'Restaurant',
         title: 'Auto Camping',
-        src:
+        image:
           'https://img.huffingtonpost.com/asset/5b9e98b83c00004c000a9c7c.jpeg?ops=scalefit_720_noupscale',
         price: '15',
         href: '#'
       },
       {
+        category: 'Restaurant',
         title: 'Pipe Room',
-        src:
+        image:
           'https://www.worldofbuzz.com/wp-content/uploads/2018/02/pork-belly-featured-image.jpg',
         price: '10',
         href: '#'
@@ -104,8 +107,4 @@ export default {
 </script>
 
 <style scoped>
-.rounded-card {
-  border-radius: 3px;
-  overflow: hidden;
-}
 </style>
