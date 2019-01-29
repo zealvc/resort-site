f441f434cfcaea39b9282f80f1aecc2e221bc77a
suusojeat <suusojeat@gmail.com>
2019-01-29 11:11:29 +0700

    resort-site-20 Sort button

---
+++ components/ChipButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-btn
+    outline
+    class="chip-btn subheading text-capitalize"
+  >
+    <slot/>
+  </v-btn>
+</template>
+
+<script>
+export default {
+  name: 'ChipButton'
+}
+</script>
+
+<style scoped>
+.chip-btn {
+  font-family: Montserrat, serif;
+  border: 1px solid #e1e7ed !important;
+  box-sizing: border-box;
+  border-radius: 3px;
+}
+</style>
--- pages/inspire.vue
+++ pages/inspire.vue
@@ -7,11 +7,13 @@
       img-src="https://cdn.vuetifyjs.com/images/cards/desert.jpg"
       go-to-link="/inspire"
     />
+    <chip-button>Guests</chip-button>
   </v-layout>
 </template>
 <script>
 import CardItemWithPrice from '../components/CardItemWithPrice'
+import ChipButton from '../components/ChipButton'
 export default {
-  components: { CardItemWithPrice }
+  components: { ChipButton, CardItemWithPrice }
 }
 </script>
