e801113eba2046a0c35125ffc41137c0ef70f47e
Somjot <somjot.pro@gmail.com>
2019-02-06 15:20:50 +0700

    Done with show all button on mobile (responsive)

--- components/ShowAllButton.vue
+++ components/ShowAllButton.vue
@@ -1,9 +1,25 @@
 <template>
   <div style="padding:0">
     <v-btn
-      class="font-weight-bold"
+      class="font-weight-bold hidden-sm-and-down"
       style="color: #006a70; margin: 0; box-shadow: none; background-color: transparent; text-transform: none; min-width: 0; padding: 0">
-      Show all
+      Show All
+      <v-icon>keyboard_arrow_right</v-icon>
+
+    </v-btn>
+    <v-btn
+
+      block
+      outline
+      class="font-weight-bold hidden-md-and-up"
+      style="
+      color: #006a70; margin: 0;
+      box-shadow: none;
+      background-color: transparent;
+      text-transform: none;
+      min-width: 0;
+      padding: 0">
+      Show All
       <v-icon>keyboard_arrow_right</v-icon>
 
     </v-btn>
@@ -12,7 +28,14 @@
 
 <script>
 export default {
-  name: 'ShowAllButton'
+  name: 'ShowAllButton',
+  data: () => ({
+    names: [
+      'Show All Accommodation',
+      'Show All Food & Drinks',
+      'Show All Solo Activities'
+    ]
+  })
 }
 </script>
 
