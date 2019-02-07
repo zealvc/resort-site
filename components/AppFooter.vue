7d23382b76baf2b32608ce9d650065011a2652fb
Somjot <somjot.pro@gmail.com>
2019-02-07 14:40:05 +0700

    Done with HomePage footer responsive with mobile.

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -2,13 +2,11 @@
   <!--TODO: Implement speed dial for persistent fab-->
   <v-footer
     height="auto"
-    style="background-color: transparent"
   >
-
-    <v-layout
-      justify-end
-      row
-
+    <v-card
+      class="flex  md12"
+      flat
+      tile
     >
       <v-bottom-sheet
         v-model="sheet">
@@ -40,20 +38,44 @@
             <v-icon size="24px">{{ icon }}</v-icon>
           </v-btn>
 
+        <v-card-title>
           <v-card-actions
             class="justify-start"
             style="color: #3D4E61;"
           >
             Copyright &copy; vKirirom Pine Resort co.,Ltd. All Rights Reserved.
           </v-card-actions>
-        </v-list>
+          <v-spacer/>
 
+          <v-btn
+            :href="href"
+            class="pa-0"
+            outline
+            flat>
+            <v-layout>
+              <v-flex 
+                class="pt-3"
+                style="padding-bottom: 15px">
+                <v-card-media
+                  :width="cardWidth"
+                  :src="image"
+                  height="33px"/>
+              </v-flex>
+              <v-flex>
+                <v-card-title
+                  primary-title>
+                  <div>
+                    <div class="text-capitalize">English</div>
+                  </div>
+                </v-card-title>
+              </v-flex>
+            </v-layout>
+          </v-btn>
 
-      </v-bottom-sheet>
+        </v-card-title>
 
-    </v-layout>
-
-
+      </v-sheet>
+    </v-card>
   </v-footer>
 
 
